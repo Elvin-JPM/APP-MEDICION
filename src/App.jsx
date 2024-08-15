ca65ef3f8765ef41de1af5364142edda887c18bf
Elvin Posadas <elvinjosuep2@gmail.com>
2024-08-15 15:31:08 -0600

    Creating the app's layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import Meters from "./pages/Meters";
 import Communications from "./pages/Communications";
 import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
 
 function App() {
   return (
@@ -11,10 +16,17 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="meters" element={<Meters />} />
-          <Route path="communications" element={<Communications />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="meters" element={<Meters />} />
+            <Route path="communications" element={<Communications />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
+
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,4 +3,3 @@ function SignUp() {
 }
 
 export default SignUp;
-SignUp;
